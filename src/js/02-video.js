689967f9553ce972e3d99514999af2a4cf766218
KulykAlexander <kulikau@gmail.com>
2022-11-29 14:31:32 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,20 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash/throttle';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+startPlay();
+player.on('timeupdate', throttle(saveTime, 1000));
+
+function saveTime() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  });
+}
+function startPlay() {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+}

c1e4d5c0b6e3515afe350206b8cd77d2f68cdc84
KulykAlexander <kulikau@gmail.com>
2022-11-29 15:04:36 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+const formEL = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('[name="email"]');
+const messageEl = document.querySelector('[name="message"]');
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+let formData = {};
+
+formEL.addEventListener('input', throttle(saveForm, 500));
+formEL.addEventListener('submit', onSubmitForm);
+
+loadForm();
+
+function saveForm(e) {
+  e.preventDefault();
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  e.target.reset();
+  console.log(
+    'Local Storage:',
+    JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))
+  );
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function loadForm() {
+  const loadData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (loadData) {
+    emailEl.value = loadData.email;
+    messageEl.value = loadData.message;
+  }
+}
